80ff77341b7d360a975391c72045c603a6fe46f8
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-20 19:50:05 +0100

    Add menu bar, and add menu__reset button to it
    Menu bar has been added initially with rough sizings, and menu__reset
    button has been added, though no functionality yet.

---
+++ script.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector(".menu");
+const menuColor = document.querySelector(".menu__color");
+const menuSize = document.querySelector(".menu__size");
+const menuReset = document.querySelector(".menu__reset");

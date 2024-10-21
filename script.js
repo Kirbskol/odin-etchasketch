40817b342197dfe025401e2c4aaea1f10f150c8c
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-21 13:57:14 +0100

    .menu__range set up with range slider
    .menu__range will be used so user can change size of grid from 16x16
    up to 100x100. Functionality not yet set up in JS, but styling has been
    implemented in style.css

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const menu = document.querySelector(".menu");
 const menuColor = document.querySelector(".menu__color");
 const menuSize = document.querySelector(".menu__size");
 const menuReset = document.querySelector(".menu__reset");
+const canvas = document.querySelector(".canvas");
+
+

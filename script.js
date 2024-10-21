2d68034faa55de9e5060466eb136aff271786c48
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-21 16:10:07 +0100

    Range value now stores value in a variable to use in js
    When range value for size of canvas is changed this is now stored in
    variable rangeValue which is used in the For loop to create grid.

--- script.js
+++ script.js
@@ -3,28 +3,31 @@ const menuColor = document.querySelector(".menu__color");
 const menuSize = document.querySelector(".menu__range");
 const menuReset = document.querySelector(".menu__reset");
 const canvas = document.querySelector(".canvas");
-let canvasSize = 16;
-let canvasSquare = [];
-let canvasNewLine = "\n";
-
 const rangeValue = document.querySelector("#rangeValue");
 const rangeSize = document.querySelector(".menu__range__size");
+let canvasSize = 16;
+let canvasSquare = [];
+let canvasRow = [];
+
+
 
 rangeSize.addEventListener("click", function() {
     rangeValue.textContent = `${rangeSize.value}x${rangeSize.value}`;
+    canvasSize = rangeSize.value;
 });
-
 
 function makeCanvas() {
     for (let x = 0; x < canvasSize; x++){
+        canvasRow[x] = document.createElement("div");
         for (let y = 0; y < canvasSize; y++){
             canvasSquare[y] = document.createElement("div");
             canvasSquare[y].style.border = "solid #000000";
             canvasSquare[y].style.borderWidth = "1px";
             canvasSquare[y].style.width = "8px";
             canvasSquare[y].style.height = "8px";
-            canvas.appendChild(canvasSquare[y]);
+            canvasRow[x].appendChild(canvasSquare[y]);
         }
+        canvas.appendChild(canvasRow[x]);
     }
 }
 
